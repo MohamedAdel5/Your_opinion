09903d125c42bd08f3368f7e5bf297f639c1b2fc
MohamedAdel5 <mouhammedadel1999@gmail.com>
2020-07-05 18:17:32 +0200

    modified static files configuration in express

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname));
 
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + "/index.html");
