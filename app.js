4aa8751111d7bce6930ac046436f49dd32b42af4
MohamedAdel5 <mouhammedadel1999@gmail.com>
2020-07-05 18:09:24 +0200

    Added static files middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use(express.static(__dirname + "/"));
+
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
